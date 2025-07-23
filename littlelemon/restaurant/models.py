600a01d7db7c8d86ed02291549e7de000150191a
allenkays <allenkays@tutanota.com>
2025-07-23 17:46:30 +0300

    Add .env file

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(default=6)
+    BookingDate= models.DateField()
+    
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.IntegerField(null=False)
+    Inventory = models.SmallIntegerField(default=10)
