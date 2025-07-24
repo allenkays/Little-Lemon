1270e41038b1d32b5b9b21351328a8f7f2dda1aa
allenkays <allenkays@tutanota.com>
2025-07-24 12:17:42 +0300

    Add django-rest-framework setup

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuView(APIView):
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    pass
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    pass
+    # queryset = MenuItem.objects.all()
+    # serializer_class = MenuItemSerializer
+
+
+# class BookingView(APIView):
+    
+    
+#     def get(self, request):
+#         items = Booking.objects.all()
+#         serializer = BookingSerializer(items, many=True)
+#         return Response(serializer.data)
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    #permission_classes = [permissions.IsAuthenticated]
